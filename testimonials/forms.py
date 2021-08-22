12deeab45fcc283d5557e0101ae860eb7bbc520f
Delroy Brown <delroybrown.db@gmail.com>
2021-08-22 22:54:09 +0100

    updating form

--- testimonials/forms.py
+++ testimonials/forms.py
@@ -9,12 +9,12 @@ from django import forms
 
 class CustomerTestimonialForm(ModelForm):
     class Meta:
-    
+
         model = Testimonial
         labels = {
             'name': '',
             'email': '',
-            'your_thoughts': '',
+            'your_thoughts': 'Tell us what you thought of the service.',
             'rating': '',
         }
         fields = (
@@ -27,7 +27,7 @@ class CustomerTestimonialForm(ModelForm):
             'name': forms.TextInput(attrs={'class': 'form-control col-md-12', 'placeholder': 'Name'}),
             'email': forms.TextInput(attrs={'class': 'form-control col-md-12', 'placeholder': 'Email'}),
             'your_thoughts': forms.Textarea(attrs={'class': 'form-control col-md-12', 'placeholder': 'Your Thoughts?'}),
-            'rating' : forms.TextInput(attrs={'type' : 'number', 'class': 'form-control col-md-3 rating-field', 'placeholder' : 'Rate us out of 5'}),
+            'rating': forms.TextInput(attrs={'type': 'number', 'class': 'form-control col-md-3 rating-field', 'placeholder': 'Rate us out of 5'}),
             # 'rating': forms.TextInput(attrs={'class': 'form-control col-md-1', 'placeholder': 'Rating'}),
 
         }
