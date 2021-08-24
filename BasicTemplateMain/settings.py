0a5037e0e7e52a78bfb1d2cba4d280f528a02689
Delroy Brown <delroybrown.db@gmail.com>
2021-08-24 19:50:45 +0100

    updated debug

--- BasicTemplateMain/settings.py
+++ BasicTemplateMain/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = os.envron.get('DEBUG')
+DEBUG = env('DEBUG')
 
 
 ALLOWED_HOSTS = ['nueui-basictemplate.herokuapp.com', 'localhost', '*']
