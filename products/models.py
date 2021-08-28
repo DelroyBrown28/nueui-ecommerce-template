fae552ce59f44c764fbef44f1df20c56e47b66de
Delroy Brown <delroybrown.db@gmail.com>
2021-08-28 17:29:21 +0100

    Added categories to prices

--- products/models.py
+++ products/models.py
@@ -38,8 +38,7 @@ class Product(models.Model):
         null=True, blank=True, upload_to='product_images', help_text='This will be the second image in the carousel.')
     category = models.ForeignKey(
         'Category', null=True, blank=True, help_text='Assign product to category', on_delete=models.CASCADE)
-    # rating = models.DecimalField(
-    #     max_digits=6, decimal_places=2, null=True, blank=True)
+
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = RichTextField()
